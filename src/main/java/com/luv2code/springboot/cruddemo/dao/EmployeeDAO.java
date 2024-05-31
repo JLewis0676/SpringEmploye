0045ad877d0e0958a8f296d6289b256a949c5594
Jordan <jordan.lewis0676@outlook.com>
2024-05-30 19:20:42 -0500

    creating data access object for Employee table

---
+++ src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAO.java
@@ -0,0 +1,24 @@
+package com.luv2code.springboot.cruddemo.dao;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public class EmployeeDAO implements IEmployeeDAO{
+    private EntityManager entityManager;
+    @Autowired
+    public EmployeeDAO(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        TypedQuery<Employee> theQuery =  entityManager.createQuery("From Employee",Employee.class);
+        List<Employee> employees = theQuery.getResultList();
+        return employees;
+    }
+}
